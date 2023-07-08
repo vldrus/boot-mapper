4d908f3d7b915ef823ef2a296de8b1b72b9e699a
Vlad <vldrus@gmail.com>
2023-07-08 09:09:26 +0300

    Initial commit

---
+++ src/main/java/bootmapper/BootMapperApplication.java
@@ -0,0 +1,13 @@
+package bootmapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bootmapper/BootMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package bootmapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
